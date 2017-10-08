ced9c695ed3d0296b67d9c11c0e88b9fe1832eeb
Allan Vialatte <allan.vialatte@icloud.com>
2017-10-08 19:02:13 +0200

    first stencil test

--- Sources/DataModelGen/main.swift
+++ Sources/DataModelGen/main.swift
@@ -3,11 +3,13 @@ import PathKit
 
 let datamodelPathArgument = Option("datamodel", Path.current.absolute().string, flag: "d", description: "The path where .xcdatamodel is")
 let outputPathOption = Option("output", Path.current.absolute().string, flag: "o", description: "The path where file will be generate")
+let templatePathOption = Option("template", Path.current.absolute().string, flag: "t", description: "The template path")
 
-let main = Commander.command(datamodelPathArgument, outputPathOption) { datamodelPath, outputPath in
+let main = Commander.command(datamodelPathArgument, outputPathOption, templatePathOption) { datamodelPath, outputPath, templatePath in
   guard let context = Context(from: datamodelPath, output: outputPath) else {
     return
   }
+  
 }
 
 main.run()

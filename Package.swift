285f4a51a46bbb84cc948a760c0d6db335726bf7
Allan Vialatte <allan.vialatte@icloud.com>
2017-10-08 16:54:32 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -4,16 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "DataModelGen",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "DataModelGen",
-            dependencies: []),
+  name: "DataModelGen",
+  dependencies: [
+    .package(url: "git@github.com:PoissonBallon/DataModelKit.git", .branch("master")),
+    .package(url: "https://github.com/onevcat/Rainbow", .branch("master")),
+    .package(url: "git@github.com:PoissonBallon/Stencil.git", .branch("master")),
+    .package(url: "https://github.com/kylef/Commander.git", .upToNextMinor(from:"0.7.1"))
+  ],
+  targets: [
+    .target(name: "DataModelGen", dependencies: ["DataModelKit", "Rainbow", ]),
     ]
 )

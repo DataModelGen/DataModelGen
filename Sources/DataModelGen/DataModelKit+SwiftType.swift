eef81c013cc33407a2fd0d73551d7698f44bc6d9
Allan Vialatte <allan.vialatte@icloud.com>
2017-11-20 08:53:13 +0100

    WIP

--- Sources/DataModelGen/DataModelKit+SwiftType.swift
+++ Sources/DataModelGen/DataModelKit+SwiftType.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import DataModelKit
+
+
+
+//def to_s
+//items = [
+//  "name=#{@name}",
+//  "type=#{@type}",
+//  "optional=#{@optional}",
+//  "default=#{@default}",
+//  "indexed=#{@indexed}"
+//]
+//"\tAttribute => " + items.join(' | ') + "\n"
+//end
+//
+//def decimal?
+//(@type == :decimal) || (@type == :double) || (@type == :float)
+//end
+//
+//def integer?
+//(@type == :integer_16) || (@type == :integer_32) || (@type == :integer_64)
+//end
+//
+//def number?
+//decimal? || integer?
+//end
+//
+//def bool?
+//@type == :boolean
+//end
+
+extension Attribute {
+  var swiftType: String {
+    switch self.type {
+    case <#pattern#>:
+      <#code#>
+    default:
+      <#code#>
+    }
+  }
+}
